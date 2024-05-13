3656f6b71753011d20ebc81cdee28684336f77ab
GalihRahdatuAnanta <galihrahdatuananta12@gmail.com>
2024-05-13 14:03:27 +0700

    Create tugaskemarin2.js

---
+++ tugaskemarin2.js
@@ -0,0 +1,30 @@
+var x="nicky";
+    x+= 15;
+    console.log(x)
+    
+    var x="adnan";
+    x+= 16;
+    console.log(x)
+    
+    var x="vallen";
+    x+= 17;
+    console.log(x)
+    
+    var x="galih";
+    x+= 18;
+    console.log(x)
+    
+    var x="bagas";
+    x+= 19;
+    console.log(x)
+    
+    var x={nama:"galih",umur:16,absen:12,kelas:"xrpl3"};
+    console.log(x)
+    console.log(x.nama)
+    console.log(x.umur)
+    console.log(x.absen)
+    console.log(x.kelas)
+    
+   
+
+      
