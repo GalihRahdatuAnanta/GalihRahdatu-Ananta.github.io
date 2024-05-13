b120b1056928ce1696b6774378fa599e38af6627
GalihRahdatuAnanta <galihrahdatuananta12@gmail.com>
2024-05-13 14:04:13 +0700

    Create tugaskemarin3.js

---
+++ tugaskemarin3.js
@@ -0,0 +1,34 @@
+//let//
+
+let x= 19 + 18
+ x= 10 + 14
+console.log(x)
+
+ x= 20 + 18
+ x+= 7 - 9
+console.log(x)
+
+x= 30 / 50
+ x-= 8 + 3
+console.log(x)
+
+x= 21 - 23
+ x!= 40 + 30
+console.log(x)
+
+x= 10 * 10
+ x/= 25 + 2
+console.log(x)
+
+//var//
+
+var y= 10 + 40
+console.log(y)
+var y= 15 + 45
+console.log(y)
+var y= 15 + 25
+console.log(y)
+var y= 10 + 30
+console.log(y)
+var y= 10 + 25
+console.log(y)
